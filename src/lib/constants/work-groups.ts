6437b71fdfc6f12a7ce70fc8be807ce5efd2545e
kadtemyazilim <kadtemyazilim@gmail.com>
2026-01-23 11:19:10 +0300

    Update work-groups.ts

--- src/lib/constants/work-groups.ts
+++ src/lib/constants/work-groups.ts
@@ -1,69 +1,74 @@
 export const SIMILAR_WORK_GROUPS = [
     {
+        value: 'A',
         label: '(A) ALT YAPI İŞLERİ',
         options: [
-            { value: 'A-I', label: 'I. GRUP: KÖPRÜ VE VİYADÜK İŞLERİ' },
-            { value: 'A-II', label: 'II. GRUP: TÜNEL İŞLERİ' },
-            { value: 'A-III', label: 'III. GRUP: BORU VE İLETİM HATTI İŞLERİ' },
-            { value: 'A-IV', label: 'IV. GRUP: İÇME-KULLANMA SUYU VE KANALİZASYON İŞLERİ' },
-            { value: 'A-V', label: 'V. GRUP: KARAYOLU İŞLERİ (Altyapı+Üstyapı)' },
-            { value: 'A-VI', label: 'VI. GRUP: DEMİRYOLU İŞLERİ (Altyapı+Üstyapı)' },
-            { value: 'A-VII', label: 'VII. GRUP: HAVAALANI İŞLERİ' },
-            { value: 'A-VIII', label: 'VIII. GRUP: BARAJLAR' },
-            { value: 'A-IX', label: 'IX. GRUP: SU YAPILARI' },
-            { value: 'A-X', label: 'X. GRUP: DENİZ YAPILARI' },
-            { value: 'A-XI', label: 'XI. GRUP: ARITMA TESİSLERİ' },
-            { value: 'A-XII', label: 'XII. GRUP: NÜKLEER ENERJİ SANTRALLERİ' },
-            { value: 'A-XIII', label: 'XIII. GRUP: HİDROELEKTRİK ENERJİ SANTRALLERİ' },
-            { value: 'A-XIV', label: 'XIV. GRUP: TERMİK SANTRALLERİ' },
-            { value: 'A-XV', label: 'XV. GRUP: RÜZGAR ENERJİ SANTRALLERİ' },
-            { value: 'A-XVI', label: 'XVI. GRUP: ENDÜSTRİYEL TESİS İNŞAATLARI' },
-            { value: 'A-XVII', label: 'XVII. GRUP: MADENCİLİK VE MADEN ÜRETİMİ İLE İLGİLİ İNŞAAT İŞLERİ' },
-            { value: 'A-XVIII', label: 'XVIII. GRUP: SAHA İŞLERİ' },
-            { value: 'A-XIX', label: 'XIX. GRUP: KABLOLU TAŞIMA SİSTEMLERİ YAPIM İŞLERİ' },
-            { value: 'A-XX', label: 'XX. GRUP: TARIMSAL ALTYAPI İŞLERİ' }
+            { value: 'I', label: 'I. GRUP: KÖPRÜ VE VİYADÜK İŞLERİ' },
+            { value: 'II', label: 'II. GRUP: TÜNEL İŞLERİ' },
+            { value: 'III', label: 'III. GRUP: BORU VE İLETİM HATTI İŞLERİ' },
+            { value: 'IV', label: 'IV. GRUP: İÇME-KULLANMA SUYU VE KANALİZASYON İŞLERİ' },
+            { value: 'V', label: 'V. GRUP: KARAYOLU İŞLERİ (Altyapı+Üstyapı)' },
+            { value: 'VI', label: 'VI. GRUP: DEMİRYOLU İŞLERİ (Altyapı+Üstyapı)' },
+            { value: 'VII', label: 'VII. GRUP: HAVAALANI İŞLERİ' },
+            { value: 'VIII', label: 'VIII. GRUP: BARAJLAR' },
+            { value: 'IX', label: 'IX. GRUP: SU YAPILARI' },
+            { value: 'X', label: 'X. GRUP: DENİZ YAPILARI' },
+            { value: 'XI', label: 'XI. GRUP: ARITMA TESİSLERİ' },
+            { value: 'XII', label: 'XII. GRUP: NÜKLEER ENERJİ SANTRALLERİ' },
+            { value: 'XIII', label: 'XIII. GRUP: HİDROELEKTRİK ENERJİ SANTRALLERİ' },
+            { value: 'XIV', label: 'XIV. GRUP: TERMİK SANTRALLERİ' },
+            { value: 'XV', label: 'XV. GRUP: RÜZGAR ENERJİ SANTRALLERİ' },
+            { value: 'XVI', label: 'XVI. GRUP: ENDÜSTRİYEL TESİS İNŞAATLARI' },
+            { value: 'XVII', label: 'XVII. GRUP: MADENCİLİK VE MADEN ÜRETİMİ İLE İLGİLİ İNŞAAT İŞLERİ' },
+            { value: 'XVIII', label: 'XVIII. GRUP: SAHA İŞLERİ' },
+            { value: 'XIX', label: 'XIX. GRUP: KABLOLU TAŞIMA SİSTEMLERİ YAPIM İŞLERİ' },
+            { value: 'XX', label: 'XX. GRUP: TARIMSAL ALTYAPI İŞLERİ' }
         ]
     },
     {
+        value: 'B',
         label: '(B) ÜSTYAPI (BİNA) İŞLERİ',
         options: [
-            { value: 'B-I', label: 'I. GRUP: BİNA İŞLERİ' },
-            { value: 'B-II', label: 'II. GRUP: BİNA İŞLERİ (Askeri, Hastane, Terminal, İbadethane vb.)' },
-            { value: 'B-III', label: 'III. GRUP: BİNA İŞLERİ (Diğer)' }
+            { value: 'I', label: 'I. GRUP: BİNA İŞLERİ' },
+            { value: 'II', label: 'II. GRUP: BİNA İŞLERİ (Askeri, Hastane, Terminal, İbadethane vb.)' },
+            { value: 'III', label: 'III. GRUP: BİNA İŞLERİ (Diğer)' }
         ]
     },
     {
+        value: 'C',
         label: '(C) SIHHİ TESİSAT VE MEKANİK TESİSAT İŞLERİ',
         options: [
-            { value: 'C-I', label: 'I. GRUP: SIHHİ TESİSAT İŞLERİ' },
-            { value: 'C-II', label: 'II. GRUP: ISITMA-SOĞUTMA, HAVALANDIRMA VE İKLİMLEME' },
-            { value: 'C-III', label: 'III. GRUP: ASANSÖR ELEKTRO-MEKANİK İŞLERİ' },
-            { value: 'C-IV', label: 'IV. GRUP: GAZ TESİSATI İŞLERİ' }
+            { value: 'I', label: 'I. GRUP: SIHHİ TESİSAT İŞLERİ' },
+            { value: 'II', label: 'II. GRUP: ISITMA-SOĞUTMA, HAVALANDIRMA VE İKLİMLEME' },
+            { value: 'III', label: 'III. GRUP: ASANSÖR ELEKTRO-MEKANİK İŞLERİ' },
+            { value: 'IV', label: 'IV. GRUP: GAZ TESİSATI İŞLERİ' }
         ]
     },
     {
+        value: 'D',
         label: '(D) ELEKTRİK İŞLERİ',
         options: [
-            { value: 'D-I', label: 'I. GRUP: ENERJİ İLETİM ŞEBEKESİ VE TESİS İŞLERİ' },
-            { value: 'D-II', label: 'II. GRUP: ENERJİ DAĞITIM ŞEBEKESİ VE TESİS İŞLERİ' },
-            { value: 'D-III', label: 'III. GRUP: ELEKTRİK ŞEBEKE VE TESİS İŞLERİ' },
-            { value: 'D-IV', label: 'IV. GRUP: 1 KV ALTI ELEKTRİK TESİSAT VE KESİNTİSİZ GÜÇ' },
-            { value: 'D-V', label: 'V. GRUP: AYDINLATMA İŞLERİ' },
-            { value: 'D-VI', label: 'VI. GRUP: ELEKTRİKLİ ULAŞIM ARAÇLARI TEKNOLOJİSİ' }
+            { value: 'I', label: 'I. GRUP: ENERJİ İLETİM ŞEBEKESİ VE TESİS İŞLERİ' },
+            { value: 'II', label: 'II. GRUP: ENERJİ DAĞITIM ŞEBEKESİ VE TESİS İŞLERİ' },
+            { value: 'III', label: 'III. GRUP: ELEKTRİK ŞEBEKE VE TESİS İŞLERİ' },
+            { value: 'IV', label: 'IV. GRUP: 1 KV ALTI ELEKTRİK TESİSAT VE KESİNTİSİZ GÜÇ' },
+            { value: 'V', label: 'V. GRUP: AYDINLATMA İŞLERİ' },
+            { value: 'VI', label: 'VI. GRUP: ELEKTRİKLİ ULAŞIM ARAÇLARI TEKNOLOJİSİ' }
         ]
     },
     {
+        value: 'E',
         label: '(E) ELEKTRONİK VE İLETİŞİM İŞLERİ',
         options: [
-            { value: 'E-I', label: 'I. GRUP: TRAFİK İZLEME VE YÖNETİM SİSTEMİ' },
-            { value: 'E-II', label: 'II. GRUP: SCADA TEKNOLOJİSİ İŞLERİ' },
-            { value: 'E-III', label: 'III. GRUP: OTOMATİK ÖLÇÜM VE GÖZLEM SİSTEMLERİ' },
-            { value: 'E-IV', label: 'IV. GRUP: ÖZDENETİMLİ BİNA VE ALAN OTOMASYON' },
-            { value: 'E-V', label: 'V. GRUP: UYDU, TV VE RADYO İLETİŞİM SİSTEMLERİ' },
-            { value: 'E-VI', label: 'VI. GRUP: GEZGİN İLETİŞİM TEKNOLOJİSİ İŞLERİ' },
-            { value: 'E-VII', label: 'VII. GRUP: SES İLETİŞİM TEKNOLOJİSİ İŞLERİ' },
-            { value: 'E-VIII', label: 'VIII. GRUP: TELEKOMÜNİKASYON OMURGA TEKNOLOJİSİ' },
-            { value: 'E-IX', label: 'IX. GRUP: GENİŞBANT ERİŞİM VE AĞ ŞEBEKESİ' }
+            { value: 'I', label: 'I. GRUP: TRAFİK İZLEME VE YÖNETİM SİSTEMİ' },
+            { value: 'II', label: 'II. GRUP: SCADA TEKNOLOJİSİ İŞLERİ' },
+            { value: 'III', label: 'III. GRUP: OTOMATİK ÖLÇÜM VE GÖZLEM SİSTEMLERİ' },
+            { value: 'IV', label: 'IV. GRUP: ÖZDENETİMLİ BİNA VE ALAN OTOMASYON' },
+            { value: 'V', label: 'V. GRUP: UYDU, TV VE RADYO İLETİŞİM SİSTEMLERİ' },
+            { value: 'VI', label: 'VI. GRUP: GEZGİN İLETİŞİM TEKNOLOJİSİ İŞLERİ' },
+            { value: 'VII', label: 'VII. GRUP: SES İLETİŞİM TEKNOLOJİSİ İŞLERİ' },
+            { value: 'VIII', label: 'VIII. GRUP: TELEKOMÜNİKASYON OMURGA TEKNOLOJİSİ' },
+            { value: 'IX', label: 'IX. GRUP: GENİŞBANT ERİŞİM VE AĞ ŞEBEKESİ' }
         ]
     }
 ];
